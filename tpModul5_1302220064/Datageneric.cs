c46065f4db71965ff0698139ee62768bf70a6cbe
Spacyes241 <farrelhaykalindonesia@gmail.com>
2024-03-17 22:25:30 +0700

    menambahkan class DataGeneric

---
+++ tpModul5_1302220064/Datageneric.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpModul5_1302220064
+{
+    internal class Datageneric<T>  
+    {
+       private T data;
+
+       public Datageneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            
+            Console.WriteLine("Data yang tersimpan adalah : " +  data);
+        }
+    }
+}
--- tpModul5_1302220064/Program.cs
+++ tpModul5_1302220064/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using tpModul5_1302220064;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nim = "1234567890";
+
+        Datageneric<string> dataGeneric = new Datageneric<string>(nim);
+
+        dataGeneric.PrintData();
+    }
+}
